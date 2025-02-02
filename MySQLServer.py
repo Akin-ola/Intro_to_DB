25d47a292a6473920f683b5c4be824e702aca833
Akin-ola <idris_akinola@yahoo.com>
2025-02-03 00:28:11 +0100

    modifies mySQLServer py file

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,12 +1,19 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "Salzroyal94"
-)
+try:
+    mydb = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "Salzroyal94"
+    )
+except mysql.connector.Error:
+    print("Failed to connect to database.")
+
 
 mycursor = mydb.cursor()
 mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
 result = mycursor.fetchone()
-print(result)
+print("Database alx_book_store created successfully!")
+
+mycursor.close()
+mydb.close()
